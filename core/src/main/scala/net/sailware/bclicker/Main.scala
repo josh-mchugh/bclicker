0faea9a3cd7d236daa7580b619c22345023191fd
Josh McHugh <josh.mchugh@mailbox.org>
2025-01-19 11:57:02 -0500

    Added SpriteBatch and render and dispose functions to Game object

--- core/src/main/scala/net/sailware/bclicker/Main.scala
+++ core/src/main/scala/net/sailware/bclicker/Main.scala
@@ -3,16 +3,27 @@ package net.sailware.bclicker
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import net.sailware.bclicker.util.FontUtil
 
 class Main extends Game:
 
+  var batch: Option[SpriteBatch] = None
   var font: Option[BitmapFont] = None
 
   override def create(): Unit =
     FontUtil.init()
     font = FontUtil.komika
+    batch = Some(SpriteBatch())
     setScreen(FirstScreen())
+
+  override def render(): Unit =
+    super.render()
+
+  override def dispose(): Unit =
+    super.dispose()
+    if batch.nonEmpty then batch.get.dispose()
+    if font.nonEmpty then font.get.dispose()
 
 class FirstScreen extends Screen:
 
